d993da92afe245f17b71f58ba1f8858f540e7491
Henrik Obsen <hto@e-train.dk>
2017-10-15 19:31:16 +0200

    section2/24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+    selector: 'app-root',
+   
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./SassTest.min.css']
 })
 export class AppComponent {
     title = 'My APP test!!!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { HttpModule } from '@angular/http';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ServerComponent
+        ServerComponent,
+        ServersComponent
     ],
     imports: [
         BrowserModule,
